dcd0e4f284e08651102acd4b0dbc936057a00c7a
Roseweltty Guida <feijaoguida@hotmail.com>
2024-04-01 09:11:32 -0300

    refact: format code

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Agendai API')
     .setDescription('The Agendai API description')
     .setVersion('1.0')
     .addTag('agendai')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('docs', app, document)
+  SwaggerModule.setup('docs', app, document);
 
-  await app.listen(8080)
+  await app.listen(8080);
 }
-bootstrap()
+bootstrap();
